5824d4565b913aa3b0f1a2bdd96b98b189307afb
Chifeng Wen <daviesx66@gmail.com>
2022-02-17 06:31:35 -0800

    Removes warnings from the VMA module.

--- renderer/vma/vk_mem_alloc.cc
+++ renderer/vma/vk_mem_alloc.cc
@@ -1,2 +1,10 @@
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wignored-qualifiers"
+#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+#pragma GCC diagnostic ignored "-Wparentheses"
+#pragma GCC diagnostic ignored "-Wreorder"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wunused-variable"
 #define VMA_IMPLEMENTATION
 #include "renderer/vma/vk_mem_alloc.h"
+#pragma GCC diagnostic pop
