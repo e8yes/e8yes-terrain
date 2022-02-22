ef0858af7e8eb5e1cd96781d6ea0de8b874f9c0d
Chifeng Wen <daviesx66@gmail.com>
2022-02-22 01:35:45 -0800

    Updates the build script to make it compile protobuf code.

--- build/build.sh
+++ build/build.sh
@@ -2,11 +2,18 @@
 
 mkdir -p bin
 
-qmake ../e8islands.pro
-make -j `nproc`
+# Protobuf code compilation.
+protoc --cpp_out=../content/proto \
+       --proto_path=../content/proto \
+       `find ../content/proto -name *.proto`
 
+# Shader code compilation.
 find ../renderer/shader -type f -exec glslc {} -o {}.spv \;
 mv ../renderer/shader/*.spv ./bin
+
+# C++ code compilation.
+qmake ../e8islands.pro
+make -j `nproc`
 
 find . -name *.so* -not -path "./bin/*" -exec cp -P {} bin/ \;
 find . -type f -executable -not -path "./bin/*" -name *main -exec cp -f {} bin/ \;
