88ea0b5e189d22c3180a938636b08ed0e56afab4
Chifeng Wen <daviesx66@gmail.com>
2022-03-25 06:07:49 -0700

    Adds a module resource ID generation and serialization/de-serialization capabilities for common primitive types.

---
+++ resource/common.cc
@@ -0,0 +1,112 @@
+/**
+ * e8yes demo web.
+ *
+ * <p>Copyright (C) 2020 Chifeng Wen {daviesx66@gmail.com}
+ *
+ * <p>This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * <p>You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <cassert>
+#include <google/protobuf/repeated_field.h>
+#include <mutex>
+#include <string>
+
+#include "common/tensor.h"
+#include "resource/common.h"
+#include "resource/proto/primitive.pb.h"
+#include "third_party/uuid/uuid4.h"
+
+namespace e8 {
+namespace {
+
+std::mutex gUuidGenMutex;
+bool gUuidInitialized = false;
+UUID4_STATE_T gUuidGenState;
+
+} // namespace
+
+Uuid GenerateUuid() {
+    std::lock_guard guard(gUuidGenMutex);
+
+    if (!gUuidInitialized) {
+        uuid4_seed(&gUuidGenState);
+        gUuidInitialized = true;
+    }
+
+    UUID4_T uuid;
+    uuid4_gen(&gUuidGenState, &uuid);
+
+    return uuid.qwords[0];
+}
+
+google::protobuf::RepeatedField<float> ToProto(vec2 const &v) {
+    google::protobuf::RepeatedField<float> proto;
+    proto.Resize(/*new_size=*/2, /*value=*/0);
+    for (unsigned i = 0; i < 2; ++i) {
+        proto[i] = v(i);
+    }
+    return proto;
+}
+
+vec2 ToVec2(google::protobuf::RepeatedField<float> const &proto) {
+    assert(proto.size() == 2);
+
+    vec2 v;
+    for (unsigned i = 0; i < 2; ++i) {
+        v(i) = proto[i];
+    }
+    return v;
+}
+
+google::protobuf::RepeatedField<float> ToProto(vec3 const &v) {
+    google::protobuf::RepeatedField<float> proto;
+    proto.Resize(/*new_size=*/3, /*value=*/0);
+    for (unsigned i = 0; i < 3; ++i) {
+        proto[i] = v(i);
+    }
+    return proto;
+}
+
+vec3 ToVec3(google::protobuf::RepeatedField<float> const &proto) {
+    assert(proto.size() == 3);
+
+    vec3 v;
+    for (unsigned i = 0; i < 3; ++i) {
+        v(i) = proto[i];
+    }
+    return v;
+}
+
+google::protobuf::RepeatedField<float> ToProto(mat44 const &m) {
+    google::protobuf::RepeatedField<float> proto;
+    proto.Resize(/*new_size=*/4 * 4, /*value=*/0);
+    for (unsigned i = 0; i < 4; ++i) {
+        for (unsigned j = 0; j < 4; ++j) {
+            proto[i + j * 4] = m(i, j);
+        }
+    }
+    return proto;
+}
+
+mat44 ToMat44(google::protobuf::RepeatedField<float> const &proto) {
+    assert(proto.size() == 4 * 4);
+
+    mat44 m;
+    for (unsigned i = 0; i < 4; ++i) {
+        for (unsigned j = 0; j < 4; ++j) {
+            m(i, j) = proto[i + j * 4];
+        }
+    }
+    return m;
+}
+
+} // namespace e8
---
+++ resource/common.h
@@ -0,0 +1,76 @@
+/**
+ * e8yes demo web.
+ *
+ * <p>Copyright (C) 2020 Chifeng Wen {daviesx66@gmail.com}
+ *
+ * <p>This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * <p>You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ISLANDS_RESOURCE_COMMON_H
+#define ISLANDS_RESOURCE_COMMON_H
+
+#include <cstdint>
+#include <google/protobuf/repeated_field.h>
+
+#include "common/tensor.h"
+#include "resource/proto/primitive.pb.h"
+
+namespace e8 {
+
+// Unique IDs to assign to each resource.
+using Uuid = int64_t;
+
+// An ID reserved for representing null values.
+Uuid const kNullUuid = 0;
+
+/**
+ * @brief GenerateUuid Generates a unique ID for every function call. This function is thread-safe.
+ */
+Uuid GenerateUuid();
+
+/**
+ * @brief ToProto Converts a 2D vector to a protobuf array.
+ */
+google::protobuf::RepeatedField<float> ToProto(vec2 const &v);
+
+/**
+ * @brief ToVec2 Converts a protobuf array back to a 2D vector. It will check if the array has
+ * exactly 2 elements. If not, it will fail.
+ */
+vec2 ToVec2(google::protobuf::RepeatedField<float> const &proto);
+
+/**
+ * @brief ToProto Converts a 3D vector to a protobuf array.
+ */
+google::protobuf::RepeatedField<float> ToProto(vec3 const &v);
+
+/**
+ * @brief ToVec3 Converts a protobuf array back to a 3D vector. It will check if the array has
+ * exactly 3 elements. If not, it will fail.
+ */
+vec3 ToVec3(google::protobuf::RepeatedField<float> const &proto);
+
+/**
+ * @brief ToProto Converts a 4 by 4 matrix to a protobuf array. The data is stored in column-major
+ * order.
+ */
+google::protobuf::RepeatedField<float> ToProto(mat44 const &m);
+
+/**
+ * @brief ToMat44 Converts a protobuf array back to a 4 by 4 matrix. It will check if the array has
+ * exactly 16 elements. If not, it will fail.
+ */
+mat44 ToMat44(google::protobuf::RepeatedField<float> const &proto);
+
+} // namespace e8
+
+#endif // ISLANDS_RESOURCE_COMMON_H
