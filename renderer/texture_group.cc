040ca7fc2214dc060f05f27cf1fc87b32388e949
Chifeng Wen <daviesx66@gmail.com>
2022-04-07 00:53:58 -0700

    Adds a module to represent a group of different types of textures.

---
+++ renderer/texture_group.cc
@@ -0,0 +1,80 @@
+/**
+ * e8yes demo web.
+ *
+ * <p>Copyright (C) 2020 Chifeng Wen {daviesx66@gmail.com}
+ *
+ * <p>This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * <p>You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <array>
+#include <cassert>
+#include <vector>
+#include <vulkan/vulkan.h>
+
+#include "renderer/pipeline_common.h"
+#include "renderer/texture_group.h"
+#include "renderer/vram.h"
+#include "resource/buffer_texture.h"
+#include "resource/common.h"
+
+namespace e8 {
+
+TextureSelector::TextureSelector(TextureGroupId const &id) : id(id), sampler(nullptr) {
+    for (unsigned i = 0; i < TEXTURE_TYPE_COUNT; ++i) {
+        textures[i] = nullptr;
+        bindings[i] = -1;
+    }
+}
+
+TextureSelector::~TextureSelector() {}
+
+bool TextureSelector::Valid() const {
+    for (unsigned i = 0; i < TEXTURE_TYPE_COUNT; ++i) {
+        if (textures[i] != nullptr) {
+            assert(bindings[i] != -1);
+
+            if (!textures[i]->Valid()) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+void TextureSelector::AppendTo(std::vector<StagingTextureBuffer const *> *target) const {
+    assert(this->Valid());
+
+    for (unsigned i = 0; i < TEXTURE_TYPE_COUNT; ++i) {
+        if (textures[i] != nullptr) {
+            target->push_back(textures[i]);
+        }
+    }
+}
+
+TextureGroup::TextureGroup() {}
+
+TextureGroup::~TextureGroup() {}
+
+bool TextureGroup::Valid() const {
+    for (unsigned i = 0; i < TEXTURE_TYPE_COUNT; ++i) {
+        if (textures[i] != nullptr) {
+            assert(bindings[i] != -1);
+
+            if (!textures[i]->Valid()) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+} // namespace e8
