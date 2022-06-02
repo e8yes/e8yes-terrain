b5a4d82594e3036101393a9e3876bb2a32cf1cb2
Chifeng Wen <daviesx66@gmail.com>
2022-06-02 02:20:48 -0700

    Adds a class to encapsulates the common states and functionality of a graphics pipeline.

---
+++ renderer/output/cached_pipeline.cc
@@ -0,0 +1,41 @@
+/**
+ * e8yes demo web.
+ *
+ * <p>Copyright (C) 2020 Chifeng Wen {daviesx66@gmail.com}
+ *
+ * <p>This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * <p>You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <functional>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "common/device.h"
+#include "renderer/basic/fixed_function.h"
+#include "renderer/basic/frame_buffer.h"
+#include "renderer/basic/pipeline.h"
+#include "renderer/basic/sampler.h"
+#include "renderer/basic/shader.h"
+#include "renderer/basic/uniform_layout.h"
+#include "renderer/basic/vertex_input.h"
+#include "renderer/output/cached_pipeline.h"
+#include "renderer/output/pipeline_output.h"
+#include "renderer/output/promise.h"
+
+namespace e8 {
+
+CachedPipelineInterface::CachedPipelineInterface(VulkanContext *context) : context_(context) {}
+
+CachedPipelineInterface::~CachedPipelineInterface() {}
+
+} // namespace e8
---
+++ renderer/output/cached_pipeline.h
@@ -0,0 +1,79 @@
+/**
+ * e8yes demo web.
+ *
+ * <p>Copyright (C) 2020 Chifeng Wen {daviesx66@gmail.com}
+ *
+ * <p>This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * <p>You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ISLANDS_RENDERER_CACHED_PIPELINE_H
+#define ISLANDS_RENDERER_CACHED_PIPELINE_H
+
+#include <memory>
+#include <vector>
+
+#include "common/device.h"
+#include "renderer/basic/fixed_function.h"
+#include "renderer/basic/frame_buffer.h"
+#include "renderer/basic/pipeline.h"
+#include "renderer/basic/sampler.h"
+#include "renderer/basic/shader.h"
+#include "renderer/basic/uniform_layout.h"
+#include "renderer/basic/vertex_input.h"
+#include "renderer/output/pipeline_output.h"
+#include "renderer/output/promise.h"
+
+namespace e8 {
+
+/**
+ * @brief The CachedPipelineInterface class Encapsulates the common states and functionality of a
+ * graphics pipeline.
+ */
+class CachedPipelineInterface {
+  public:
+    CachedPipelineInterface(VulkanContext *context);
+    virtual ~CachedPipelineInterface();
+
+    /**
+     * @brief Launch Launches a graphics pipeline, and let it run asynchronously on the GPU.
+     *
+     * @param instance_id The zero-offset index of the instanced call. This function may be called
+     * multiple times with the same dependent inputs. Each of these calls is considered an instance.
+     * @param inputs Outputs of dependent parents.
+     * @param prerequisites The set of promises that need to be resolved before running this
+     * pipeline.
+     * @param completion_signal_count The number of signals to emit when the pipeline is complete.
+     * @param output Stores the output of this pipeline.
+     * @return The GPU operation's promised fulfillment.
+     */
+    virtual Fulfillment Launch(unsigned instance_id,
+                               std::vector<PipelineOutputInterface *> const &inputs,
+                               std::vector<GpuPromise *> const &prerequisites,
+                               unsigned completion_signal_count,
+                               PipelineOutputInterface *output) = 0;
+
+  protected:
+    // Contextual Vulkan handles.
+    VulkanContext *context_;
+
+    // Common pipeline states which are usually populated on construction.
+    std::unique_ptr<ShaderStages> shader_stages_;
+    std::unique_ptr<ShaderUniformLayout> uniform_layout_;
+    std::unique_ptr<VertexInputInfo> vertex_inputs_;
+    std::unique_ptr<FixedStageConfig> fixed_stage_config_;
+    std::unique_ptr<GraphicsPipeline> pipeline_;
+    std::unique_ptr<ImageSampler> texture_sampler_;
+};
+
+} // namespace e8
+
+#endif // ISLANDS_RENDERER_CACHED_PIPELINE_H
